005c1b891373903c0552e5c77ce7643ce9e255e1
Parth Patel <parth6796@yahoo.com>
2021-10-06 11:16:53 +0100

    added the basic mvp for views

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import modelName
-from .forms import formName
+from .forms import ShortenerForm
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        
+    if request.method == "POST":
+        form = ShortenerForm(request.POST)
+        if form.is_valid():
+            return render(request, "index.html")
+
+
+"""Need to add a way to save url here, and an else to redirect if invalid url
+"""
