1659c2dca57b0793ca7f073e665baa9e7b7dc066
Parth Patel <parth6796@yahoo.com>
2021-10-06 11:01:21 +0100

    added html file template

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import modelName
+from .forms import formName
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        
