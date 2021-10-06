e51f98c79375b97f9363696f240ddb4788092136
Parth Patel <parth6796@yahoo.com>
2021-10-06 12:53:57 +0100

    added init to forms

--- urlshortener/shortener/forms.py
+++ urlshortener/shortener/forms.py
@@ -3,7 +3,10 @@ from . import models
 
 
 class ShortenerForm(forms.ModelForm):
-
     class Meta:
         model = models.URL
-        fields = ['long']
+        fields = ["long"]
+
+    def __init__(self, *args, **kwargs):
+        super(ShortenerForm, self).__init__(*args, **kwargs)
+        self.fields["long"].label = "What you wanna shorten?"
