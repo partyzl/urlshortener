0d70a9b0d754330227f497f1ffc06be97d8e25b1
Parth Patel <parth6796@yahoo.com>
2021-10-06 13:01:19 +0100

    added logic for retrieving id

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -5,10 +5,20 @@ from .forms import ShortenerForm
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        form = ShortenerForm(request.POST)
-        if form.is_valid():
-            return render(request, 'index.html', {"form": ShortenerForm}) # Add the correct model stuff here
-    return render(request, 'index.html', {"form": ShortenerForm})
+        given_url = ShortenerForm(request.POST)
+        if given_url.is_valid():
+            url = given_url.save()
+            shortener = ShortenerForm()
+            context = {
+                "form": shortener,
+                "shortened": f"http://localhost:8000/{url.id}",
+            }
+            return render(
+                request, "index.html", context
+            )  # Add the correct model stuff here
+    shortener = ShortenerForm()
+    context = {"form": shortener, "shortened": ""}
+    return render(request, "index.html", context)
 
 
 """Need to add a way to save url here, and an else to redirect if invalid url
