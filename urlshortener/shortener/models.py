06730eceea3b32d4aa07f7a6f2358a4cc3c505bf
Parth Patel <parth6796@yahoo.com>
2021-10-06 13:17:52 +0100

    specified models.charField

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -3,8 +3,7 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 class URL(models.Model):
-
     def __str__(self):
         return self.long
 
-    long = CharField(max_length=200)
+    long = models.CharField(max_length=200)
